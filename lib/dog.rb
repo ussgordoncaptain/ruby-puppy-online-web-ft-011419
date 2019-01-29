f9e5d1c71837bc5652a3d1c6f112115f1277c318
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-29 11:11:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog 
   @@all = []
+  @@names = []
   def initialize(name)
     @name = name
     @@all << self
+    @@names << name
   end
   attr_accessor :name
   def self.all
-    puts @@all
+    puts @@names
   end
   def self.clear_all
     @@all = []
